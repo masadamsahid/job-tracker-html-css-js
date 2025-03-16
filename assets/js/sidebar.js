c9afd790473f79fb9d3ca93448663740a025ca01
Noora Aulia H <nooraaulia20h@gmail.com>
2025-03-17 05:37:31 +0700

    responsive-layout-app-details

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,4 +1,3 @@
-
 const sidebar = document.getElementById('sidebar');
 const overlay = document.getElementById('overlay');
 
@@ -6,19 +5,20 @@ const sidebarTrigger = document.getElementById('sidebar-trigger');
 const sidebarCloser = document.getElementById('sidebar-closer');
 
 function openSidebar() {
-  sidebar.classList.add('sidebar-mobile-opened');
-  overlay.style.display = 'block';
+  sidebar.classList.add('sidebar-mobile-opened'); // Menambahkan kelas untuk membuka sidebar
+  overlay.style.display = 'block'; // Menampilkan overlay
 }
 
-sidebarTrigger.addEventListener('click', openSidebar);
+sidebarTrigger.addEventListener('click', openSidebar); // Menambahkan event listener untuk membuka sidebar
 
 function closeSidebar() {
-  overlay.style.display = 'none';
-  sidebar.classList.remove('sidebar-mobile-opened');
+  overlay.style.display = 'none'; // Menyembunyikan overlay
+  sidebar.classList.remove('sidebar-mobile-opened'); // Menghapus kelas untuk menutup sidebar
 }
 
-sidebarCloser.addEventListener('click', closeSidebar);
-overlay.addEventListener('click', closeSidebar);
+sidebarCloser.addEventListener('click', closeSidebar); // Menambahkan event listener untuk menutup sidebar
+overlay.addEventListener('click', closeSidebar); // Menambahkan event listener untuk menutup sidebar saat overlay diklik
+
 window.addEventListener('resize', () => {
-  if (window.innerWidth >= 1024) closeSidebar();
+  if (window.innerWidth >= 1024) closeSidebar(); // Menutup sidebar jika lebar jendela >= 1024px
 });
